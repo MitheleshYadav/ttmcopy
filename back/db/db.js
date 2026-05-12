44b7fa14b781a96ce5b89fa94853a411dd9fc634
MitheleshYadav <mithelesh942@gmail.com>
2026-05-12 09:56:25 +0530

    coonection done

--- back/db/db.js
+++ back/db/db.js
@@ -1,11 +1,14 @@
-import mongoose from "mongoose";
-
+const mongoose = require("mongoose");
 
 const connectDb = async ()=>{
     try {
-        await mongoose.connect(`${process.env.mongo_url}`)
+        await mongoose.connect(`${process.env.mongo_url}/mydatabase`)
+        console.log("COnnection done!!!!")
     } catch (error) {
         console.log("error :- ", error)
         process.exit(1);
     }
 }
+
+module.exports = connectDb;
+
--- back/index.js
+++ back/index.js
@@ -1,7 +1,14 @@
 const express = require("express");
-const cors = require("cors")
+const cors = require("cors");
+const connectDb  = require("./db/db");
 const app = express();
+const dotenv = require('dotenv');
 
+dotenv.config({
+  path : "./.env"
+})
+connectDb();
+console.log(connectDb)
 app.use(express.json());
 app.use(cors());
 app.get("/", (req, res) => {
