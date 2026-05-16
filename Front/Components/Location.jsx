46c8972ef6923d4742a5f8607290f1987b48fb32
MitheleshYadav <mithelesh942@gmail.com>
2026-05-16 10:04:56 +0530

    now going to work on the user branch in order to get the location func

---
+++ Front/Components/Location.jsx
@@ -0,0 +1,11 @@
+import { MapContainer } from "react-leaflet"
+
+function Location(){
+    return (
+        <MapContainer center={[51.505, -0.09]} zoom={13}>
+            
+        </MapContainer>
+    )
+}
+
+export default Location;
